e7ba21aafc796f7aedef6a1cd94aa74c1a05df33
Antony Lemos <lemosantony@gmail.com>
2020-08-12 16:37:57 -0300

    feat: query params with http module

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -2,6 +2,16 @@ const users = require('../mocks/users');
 
 module.exports = {
   listUsers(request, response) {
+    const { order } = request.query;
+
+    const sortedUsers = users.sort((previous, current) => {
+      if (order === 'desc') {
+        return previous.id < current.id ? 1 : -1;
+      }
+
+      return previous.id > current.id ? 1 : -1;
+    });
+
     response.writeHead(200, { 'Content-Type': 'application/json' });
     response.end(JSON.stringify(users));
   },
--- src/index.js
+++ src/index.js
@@ -1,26 +1,25 @@
 const http = require('http');
+const url = require('url');
 
 const routes = require('./routes');
 
 const server = http.createServer((request, response) => {
-  console.log(`Request method: ${request.method} | Endpoint: ${request.url}`);
+  const parsedUrl = url.parse(request.url, true);
+
+  console.log(`Request method: ${request.method} | Endpoint: ${parsedUrl.pathname}`);
 
   const route = routes.find((routeObject) => (
-    routeObject.endpoint === request.url && routeObject.method === request.method
+    routeObject.endpoint === parsedUrl.pathname && routeObject.method === request.method
   ));
 
   if (route) {
+    request.query = parsedUrl.query;
+
     route.handler(request, response);
   } else {
     response.writeHead(404, { 'Contant-Type': 'text/html' });
-    response.end(`Cannot ${request.method} ${request.url}`);
+    response.end(`Cannot ${request.method} ${parsedUrl.pathname}`);
   }
-
-  // if (request.url === '/users' && request.method === 'GET') {
-  //   UserController.listUsers(request, response);
-  // } else {
-    
-  // }
 });
 
 server.listen(3333, () => console.log('⚡ Server started at http://localhost:3333'));
